97e71135d4cb488f0e432ebdfe223b426c6f41c0
tq139 <tq1394800054@gmail.com>
2022-09-01 15:23:49 +1000

    feat/react router 改为异步请求

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { asyncComponent } from './components/AsyncComponent';
 import Home from './components/Home/index';
+import Navbar from './components/NavBar';
 import Login from './components/Login';
-import NotFoundPage from './components/NotFoundPage/index'
-import Datav from './components/Datavisualization/index'
+
+
 import { TOP_ROUTE } from './constants/path';
 import './index.css'
+// 全局导入数据可视乎主题
 import { G2 } from '@ant-design/plots'
 import themeOptions from './g2-theme.json'
-import Navbar from './components/NavBar';
+
+// 异步导入
+const Datav = asyncComponent(() => import("./components/Datavisualization/index"))
+
+import NotFoundPage from './components/NotFoundPage/index'
+// import Datav from './components/Datavisualization/index'
 
 const { registerTheme } = G2;
-
-
-
-
-console.log(themeOptions)
 registerTheme('custom-theme', themeOptions);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
